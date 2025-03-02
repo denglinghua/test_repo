531f998df497bc3963a00f78dc70f476fa2f0ce1
deng <denglinghua@outlook.com>
2025-03-02 12:19:06 -0400

    add export tooltips

--- web/src/components/ExportSection.js
+++ web/src/components/ExportSection.js
@@ -1,18 +1,27 @@
-import React from "react";
-import { Container, Typography, Box, Button, Input } from "@mui/material";
-import * as XLSX from 'xlsx';
-import { saveAs } from 'file-saver';
+import React, { useState } from "react";
+import { Container, Typography, Box, Button, Snackbar, Alert } from "@mui/material";
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
 
 const ExportSection = ({ file }) => {
+  const [open, setOpen] = useState(false);
+
   const handleExport = () => {
     const worksheet = XLSX.utils.json_to_sheet(transformData(file.rows));
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
 
-    const excelBuffer = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
-    const blob = new Blob([excelBuffer], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8" });
+    const excelBuffer = XLSX.write(workbook, {
+      bookType: "xlsx",
+      type: "array",
+    });
+    const blob = new Blob([excelBuffer], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8",
+    });
 
     saveAs(blob, "export.xlsx");
+
+    setOpen(true);
   };
 
   const transformData = (rows) => {
@@ -22,37 +31,42 @@ const ExportSection = ({ file }) => {
       const newEval = {
         "Clinical Accuracy": evaluation.accuracyRating,
         "Overall Quality": evaluation.qualityRating,
-        "Comment": evaluation.comment
-      }
-      return { ...rest, ...newEval};
+        Comment: evaluation.comment,
+      };
+      return { ...rest, ...newEval };
     });
-  }
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   return (
     <Container maxWidth="md">
-      <Box
-        display="flex"
-        flexDirection="column"
-        alignItems="left"
-        justifyContent="center"
-      >
-        <Typography
-          variant="body1"
-          gutterBottom
-          style={{ marginBottom: "2rem" }}
-        >
+      <Box display="flex" flexDirection="column" alignItems="left" justifyContent="center">
+        <Typography variant="body1" gutterBottom style={{ marginBottom: "2rem" }}>
           All records have been successfully scored. You can now export the results.
         </Typography>
 
-        <Button
-          variant="contained"
-          color="primary"
-          sx={{ width: "200px" }}
-          onClick={handleExport}
-        >
+        <Button variant="contained" color="primary" sx={{ width: "200px" }} onClick={handleExport}>
           Export to Excel
         </Button>
       </Box>
+
+      <Snackbar
+        open={open}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        autoHideDuration={3000}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          Export successful!
+        </Alert>
+      </Snackbar>
     </Container>
   );
 };
